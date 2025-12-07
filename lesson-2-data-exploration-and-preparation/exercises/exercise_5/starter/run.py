707120cac6d7b7dd2dd5dbd11a12d47076aee8c4
SWStribling <sstri005@gmail.com>
2025-12-07 16:40:16 -0600

    bug fix: typos in exercise 5

--- lesson-2-data-exploration-and-preparation/exercises/exercise_5/starter/run.py
+++ lesson-2-data-exploration-and-preparation/exercises/exercise_5/starter/run.py
@@ -6,6 +6,16 @@ import wandb
 
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+#!/usr/bin/env python
+import argparse
+import logging
+import os
+
+import pandas as pd
+import wandb
+
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
 
 
@@ -13,32 +23,38 @@ def go(args):
 
     run = wandb.init(project="exercise_5", job_type="process_data")
 
-    ## YOUR CODE HERE
     logger.info("Downloading artifact")
     artifact = run.use_artifact(args.input_artifact)
     artifact_path = artifact.file()
 
-    logger.info("Reading data into a DataFrame")
     df = pd.read_parquet(artifact_path)
-    
-    logger.info("Preprocessing data")
-    # Example preprocessing: drop rows with missing values
-    df = df.dropduplicates().reset_index(drop=True)
 
-    df['title'] = df['title'].fillna(value = "", inplace = True)
-    df['song_name'] = df['song_name'].fillna(value = "", inplace = True)
-    df['text_feature'] = df['title'] + " " + df['song_name']
+    # Drop the duplicates
+    logger.info("Dropping duplicates")
+    df = df.drop_duplicates().reset_index(drop=True)
 
-    outfile = args.artifact_name
-    df.to_csv(outfile)
+    logger.info("Fixing missing values")
+    # These are missing values that are due to an old version of the data. On new data,
+    # because of a change in the web form used to register new songs, the title and the
+    # song name are already empty strings
+    df['title'].fillna(value='', inplace=True)
+    df['song_name'].fillna(value='', inplace=True)
+    df['text_feature'] = df['title'] + ' ' + df['song_name']
+
+    filename = "processed_data.csv"
+    df.to_csv(filename)
+
     artifact = wandb.Artifact(
         name=args.artifact_name,
         type=args.artifact_type,
-        description=args.artifact_description
+        description=args.artifact_description,
     )
-    artifact.add_file(outfile)
+    artifact.add_file(filename)
 
+    logger.info("Logging artifact")
     run.log_artifact(artifact)
+
+    os.remove(filename)
 
 
 if __name__ == "__main__":
